4b110c2566b9edaa9c4cc9e1f001eaa8b5f6573e
Maksad Khalipaev <maks14flay@gmail.com>
2023-08-10 06:01:24 +0500

    change api key

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API_URL = "https://api.kinopoisk.dev/";
-export const API_KEY = "V1QV0JZ-NMQ4PBV-QMDJYP3-ZC212CQ";
+export const API_KEY = "F37HY9W-CTD4G9Q-P7CY00J-SRM59AW";
 // export const API_KEY = "F37HY9W-CTD4G9Q-P7CY00J-SRM59AW";
 export const VIDEOS_API = "https://voidboost.net/embed/";
 export const VIDEOS_POSTER = "?poster=1&poster_id=4&df=1";
