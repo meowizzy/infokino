7245912c6bcf4f3c958eb892f67b8ed2f031ba39
Maksad Khalipaev <apple@appleui-MacBookAir.local>
2024-06-06 07:25:24 +0500

    fix media source constant

--- src/app/constants.js
+++ src/app/constants.js
@@ -4,8 +4,8 @@ export const KINOPOISK_API_VERSIONS = {
      API_VER_1_2: "v1.2",
      API_VER_1_4: "v1.4",
 }
-export const MEDIA_URL = process.env.REACT_MEDIA_SOURCE;
-export const MEDIA_POSTER = process.env.REACT_MEDIA_POSTER_PARAMS;
+export const MEDIA_URL = process.env.REACT_APP_MEDIA_SOURCE;
+export const MEDIA_POSTER = process.env.REACT_APP_MEDIA_POSTER_PARAMS;
 export const LOCAL_STORAGE_AUTH = "LOCAL_STORAGE_AUTH";
 
 
